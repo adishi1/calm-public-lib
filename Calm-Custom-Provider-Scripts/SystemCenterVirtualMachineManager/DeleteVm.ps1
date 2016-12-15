2fe8d070284bc557c61aa3a653c6fd8d7b86f862
kumarsarath588 <kumar.sarath588@gmail.com>
2016-12-15 12:24:27 +0530

    more smooth scvmm delete vm

--- Calm-Custom-Provider-Scripts/SystemCenterVirtualMachineManager/DeleteVm.ps1
+++ Calm-Custom-Provider-Scripts/SystemCenterVirtualMachineManager/DeleteVm.ps1
@@ -4,6 +4,5 @@
 #description      "This script deletes VM in system center virtual machine manager"
 
 Import-Module "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\psModules\virtualmachinemanager\virtualmachinemanager.psd1"
-get-vm -name "@@{VM_NAME}@@" | Stop-SCVirtualMachine
-Remove-SCVirtualMachine -VM "@@{VM_NAME}@@" -Force
-Remove-VM -VM "@@{VM_NAME}@@" -Force
+Get-SCVirtualMachine -Name "@@{VM_NAME}@@" | Stop-SCVirtualMachine -Force
+Get-SCVirtualMachine -Name "@@{VM_NAME}@@" | Remove-SCVirtualMachine -Force
